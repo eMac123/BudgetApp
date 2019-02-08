1a9be6120c30d724a5f6a4034e21c82de1bc9997
Ignas <irocys@gmail.com>
2019-02-08 22:14:07 +0200

    added clearInput, Data, addToData

--- js/budget.js
+++ js/budget.js
@@ -1,8 +1,9 @@
-// create constructor inc and exp
-// create data structure
-// create method to store items in to he data
+
 // create method to store everyting within localstorage
+
 // create method to calculate percentage of the budget
+
+
 // for week, month, each income/expen percentage comapring with all abudget
 // create method to clear input values
 // check if amount isn't NaN
@@ -16,12 +17,14 @@
 //    USER INTERFACE CONTROLLER
 const UIController = (function() {
 
+  
+
     const DOM =  {
         AddIncome: {
             incomCont: document.getElementById('addInc'),
             incomDescrip: document.getElementById('incExp'),
             incomAmount: document.getElementById('incAmount'),
-            incomType: document.getElementById('incType'),
+        
             incomBtn: document.getElementById('incomBtn'),
             incomeList: document.getElementById('incomeList')
         },
@@ -29,7 +32,7 @@ const UIController = (function() {
             expCont: document.getElementById('addExp'),
             expDescrip: document.getElementById('expDec'),
             expAmount: document.getElementById('expAmount'),
-            expType: document.getElementById('expType'),
+            
             expBtn: document.getElementById('expenBtn'),
             expensiveList: document.getElementById('expensiveList')
         },
@@ -37,10 +40,11 @@ const UIController = (function() {
             listItem: '<li class="collection-item">%des% <span>$ %mon%</span><i class ="fas fa-trash-alt" id="delete"></i> </li>'
         }
 
-    }
+    };
 
 
     return {
+
         
         publicDOM: function() {
             return DOM;
@@ -53,17 +57,85 @@ const UIController = (function() {
             newHTML = html.replace('%des%', domValue.description);
             newHTML = newHTML.replace('%mon%', domValue.amount);
             domObj.insertAdjacentHTML('beforeend', newHTML);             
+        },
+
+        clearInputs: function (type) {
+            if(type === 'income') {
+                DOM.AddIncome.incomDescrip.value = '';
+                DOM.AddIncome.incomAmount.value = '';
+            } else if(type === 'expen') {
+                DOM.AddExpensive.expDescrip.value = '';
+                DOM.AddExpensive.expAmount.value = '';
+            }
         }
         
     }
 })();
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //    BUDGET CONTROLLER
 const BudgetController = (function() {
 
+      const inExp = function (id, type, amount, des) {
+          this.id = id;
+          this.type = type;
+          this.amount = amount;
+          this.description = des;
+      }
+
+
+     const DATAstructure = {
+         income: {
+             items: [],
+             all: 0,
+             perc: 0
+         },
+         expen: {
+             items: [],
+             all: 0,
+             perc: 0
+         }
+     }
+
+
 
     return {
+
+        test: function() {
+            return DATAstructure;
+        },
+
+         createItem: function (type, amount, des) {
+             let item, id;
+
+
+
+             id = des;
+
+             item = new inExp(id, type, amount, des);
+
+             DATAstructure[type].items.push(item);
+
+             
+             return item;
+         },
+
         // get input values from income
         collectValue: function(obj) {
             return {
@@ -82,7 +154,7 @@ const BudgetController = (function() {
 
             }
         }
-    }
+    }      
 
 })();
 
@@ -124,36 +196,49 @@ const GlobalController = (function(UI,BUDGET) {
 
     // addEventListener
     const addEvent = function() {
-        document.getElementById('toDo').addEventListener('click', function(e) {
-                   console.log()
-                    
-                   // if e.target === income button then add item to the income UI
-                   if (e.target.classList.contains('incomeBtn')) {
-
-                       // get inc input value
-                       let inpValue = BUDGET.collectValue(inputVal);                       
-                       UI.addItem(HTML, listIncCont, inpValue);
-                  // if e.target === exp button then add item to the exp UI
-                   } else if (e.target.classList.contains('expensBtn')){
-
-                       // get exp input value
-                       let inpValueExp = BUDGET.collectValueEx(expVal);
-                       UI.addItem(HTML, listExpCont, inpValueExp);
-                       
-
-                       
-                   }
-                   
-        });
+        document.getElementById('toDo').addEventListener('click', makeItem)
     }
+
+
+
+
 
 
      // function to add item to the UI, clear inputs, add item to the data structure, check if 
      // there isn't any empty values if there is ane empty value then throw error
      // when the item was added to the UI clear input values
      // also store everythinf to the localstorage
+     const makeItem = function(e) {
+         let type, item;
 
-     
+// 1. Add item to the UI
+         
+         if (e.target.classList.contains('incomeBtn')) {               // if e.targte === income button then add item to the income UI
+             // get inc input value
+             let inpValue = BUDGET.collectValue(inputVal);
+             UI.addItem(HTML, listIncCont, inpValue);
+
+// 2. Add item to the DATA INCOME
+            type = 'income';
+            item = BUDGET.createItem(type,inpValue.amount, inpValue.description)
+            UI.clearInputs(type); // clear input value
+            
+
+            
+         } else if (e.target.classList.contains('expensBtn')) {          // if e.target === exp button the nadd item to the exp UI  
+            // get exp input value
+             let inpValueExp = BUDGET.collectValueEx(expVal);
+             UI.addItem(HTML, listExpCont, inpValueExp);
+
+// 2.Add item to the DATA EXPENSE
+            type = 'expen';
+            item = BUDGET.createItem(type,inpValueExp.amount, inpValueExp.description);
+            UI.clearInputs(type); // clear input value
+
+         }
+
+         
+     };
 
 
 
